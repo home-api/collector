bffe84c1552ef9664b94951c8f8c6f035958db6a
Yegor Korobitsin <egor.korobitsin@itechart-group.com>
2016-12-27 18:02:19 +0300

    add junit5 support

---
+++ src/test/java/repository/impl/InMemoryOrderRepositoryTest.java
@@ -0,0 +1,13 @@
+package repository.impl;
+
+import org.junit.gen5.api.Assertions;
+import org.junit.gen5.api.Test;
+
+public class InMemoryOrderRepositoryTest {
+
+    @Test
+    public void first() {
+        Assertions.assertNull("");
+    }
+
+}
